1842c9c43d631675f86f81c7e7c3c7b973890aee
Tom Slater <tjaslater@gmail.com>
2018-03-27 16:03:05 +0100

    Install update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # edx_abs
 Python scripts for performing absorption correction on 3D STEM-EDX data
+
+Installation
+The main dependency for the package is pycuda. Full installation instructions 
+for pycuda can be found at https://wiki.tiker.net/PyCuda/Installation/Windows.
+
+Once pycuda has been successfully installed this should be easy to install and 
+run via pip or setuptools.
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
 setup(name='edx_abs',
-	  version='0.1',
-	  description='Python scripts for performing absorption correction on 3D STEM-EDX data',
-	  url='http://github.com/TomSlater/edx_abs',
-	  author='Tom Slater',
-	  author_email='tjaslater@gmail.com',
-	  license='GPL-3.0+',
-	  packages=['edx_abs'],
-	  zip_safe=False)
+      version='0.1',
+      description='Python scripts for performing absorption correction on 3D STEM-EDX data',
+      url='http://github.com/TomSlater/edx_abs',
+      author='Tom Slater',
+      author_email='tjaslater@gmail.com',
+      license='GPL-3.0+',
+      packages=['edx_abs'],
+      install requires=[
+              'numpy',
+              'scipy',
+              'skimage',
+              'matplotlib',
+              'astra-toolbox',
+              ],
+      zip_safe=False)
