958cc879d7802f7be60513c60e2bfba072cc8e2c
Tom Slater <TomSlater@users.noreply.github.com>
2018-03-27 14:48:12 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='edx_abs',
+	  version='0.1',
+	  description='Python scripts for performing absorption correction on 3D STEM-EDX data',
+	  url='http://github.com/TomSlater/edx_abs',
+	  author='Tom Slater',
+	  author_email='tjaslater@gmail.com',
+	  license='GPL-3.0+',
+	  packages=['edx_abs'],
+	  zip_safe=False)
